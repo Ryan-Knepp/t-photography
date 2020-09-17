a70c743e744a3c1f271de3f7194ecac330a1cb93
Ryan Knepp <rknepp86@gmail.com>
2020-09-16 22:48:01 -0500

    initial setup

---
+++ gridsome.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  templates: {
+    // Add templates for content types here.
+    // Read more: https://gridsome.org/docs/templates/
+  },
+  plugins: [
+    {
+      use: "@gridsome/source-contentful",
+      options: {
+        space: process.env.CONTENTFUL_SPACE_ID, // required
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN, // required
+        typeName: "Contentful",
+      },
+    },
+    {
+      use: "gridsome-plugin-tailwindcss",
+    },
+  ],
+};
