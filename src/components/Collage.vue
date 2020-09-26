ab09233bc25dc0eaf7488bdd3c406afdf8fb46fa
Ryan Knepp <rknepp86@gmail.com>
2020-09-26 16:47:01 -0500

    adding collage component

---
+++ src/components/Collage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-screen grid grid-cols-3">
+    <div>Family Gallery</div>
+    <g-image :src="$static.data.heroCollageImage1.file.url" />
+    <g-image :src="$static.data.heroCollageImage3.file.url" />
+    <g-image :src="$static.data.heroCollageImage2.file.url" />
+    <div>Newborn Gallery</div>
+  </div>
+</template>
+
+<static-query>
+query {
+    data: contentfulHomePage (id: "31FUIVsozzfUPvNHZENeJP") {
+        id
+        heroCollageImage1 {
+            title
+            file {
+                url
+            }
+        }
+        heroCollageImage2 {
+            title
+            file {
+                url
+            }
+        }
+        heroCollageImage3 {
+            title
+            file {
+                url
+            }
+        }
+      }
+  }
+</static-query>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="w-full">
     <nav-bar />
+    <collage />
     <slot />
   </div>
 </template>
 
 <script>
 import NavBar from "../components/NavBar.vue";
+import Collage from "../components/Collage.vue"
 
 export default {
   components: {
     NavBar,
+    Collage,
   },
 };
 </script>
